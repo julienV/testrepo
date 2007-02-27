959bbd93ae14eac4e24e7ffc58cfcdff24794cef
schlu <schlu@users.sourceforge.net>
2007-02-27 14:52:16 +0000

    cleanup

--- admin/views/help/tmpl/default.php
+++ admin/views/help/tmpl/default.php
@@ -11,7 +11,7 @@
 <table cellpadding="4" cellspacing="0" border="0" width="100%" class="adminlist">
 	<tr>
 		<td align="left">
-			<strong><?php echo JText::_( 'Search' ); ?>:</strong>
+			<strong><?php echo JText::_( 'Search' ); ?></strong>
 			<input class="text_area" type="hidden" name="option" value="com_eventlist" />
 			<input type="text" name="search" id="search" value="<?php echo $this->helpsearch;?>" class="inputbox" />
 			<input type="submit" value="<?php echo JText::_( 'Go' ); ?>" class="button" />
