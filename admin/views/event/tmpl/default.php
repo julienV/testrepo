90100133a39607b8aeed4fddab39f3efec14e760
schlu <schlu@users.sourceforge.net>
2007-10-02 19:25:22 +0000

    bugfix

--- admin/views/event/tmpl/default.php
+++ admin/views/event/tmpl/default.php
@@ -327,7 +327,7 @@ defined('_JEXEC') or die('Restricted access');
 					<tr id="counter_row" style="display:none;">
 						<td><?php echo JText::_( 'RECURRENCE COUNTER' ); ?>:</td>
 						<td>
-					        <?php echo JHTML::_('calendar', $this->row->enddates, "recurrence_counter", "recurrence_counter"); ?>
+					        <?php echo JHTML::_('calendar', $this->row->recurrence_counter, "recurrence_counter", "recurrence_counter"); ?>
 					        <span class="editlinktip hasTip" title="<?php echo JText::_('FORMAT DATE'); ?>::<?php echo JText::_('RECURRENCE COUNTER TIP'); ?>">
 								<?php echo $infoimage; ?>
 							</span>
