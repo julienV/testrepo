c933a62cdaff71741466d9883ea96bac5c511742
schlu <schlu@users.sourceforge.net>
2007-10-09 18:18:10 +0000

    updated version

--- admin/models/updatecheck.php
+++ admin/models/updatecheck.php
@@ -93,7 +93,7 @@ class EventListModelUpdatecheck extends JModel
 			$_updatedata->changes 		= explode(';', $data[6]);
 			$_updatedata->failed 		= 0;
 
-			$_updatedata->current = version_compare( '0.9.0.0.dev', $_updatedata->version );
+			$_updatedata->current = version_compare( '0.9.0.0.dev2', $_updatedata->version );
 
 		}
 
