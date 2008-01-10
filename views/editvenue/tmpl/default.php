082961e5c0b9386aaee2440db790d83283b780d4
spook <spook68@users.sourceforge.net>
2008-01-10 18:12:57 +0000

    editvenue view updated

--- views/editvenue/tmpl/default.php
+++ views/editvenue/tmpl/default.php
@@ -107,183 +107,180 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
   	}
 </script>
 
-<form enctype="multipart/form-data" name="adminForm" id="adminForm" action="<?php echo JRoute::_('index.php') ?>" method="post" class="form-validate">
 
-	<table class="adminform" width="100%">
-		<tr>
-			<td>
-				<div style="float: left;">
-					<label for="venue">
-						<?php echo JText::_( 'VENUE' ).':'; ?>
-					</label>
-					<input class="inputbox required" type="text" name="venue" id="venue" value="<?php echo $this->escape($this->row->venue); ?>" size="55" maxlength="50" />
-					&nbsp;&nbsp;&nbsp;
+<div id="eventlist" class="el_editvenue">
+
+    <?php if ($this->params->def( 'show_page_title', 1 )) : ?>
+    <h1 class="componentheading">
+        <?php echo $this->params->get('page_title'); ?>
+    </h1>
+    <?php endif; ?>
+
+    <form enctype="multipart/form-data" name="adminForm" id="adminForm" action="<?php echo JRoute::_('index.php') ?>" method="post" class="form-validate">
+
+        <div class="el_save_buttons floattext">
+  				<button type="button" onclick="return submitbutton('savevenue')">
+  					<?php echo JText::_('SAVE') ?>
+  				</button>
+  				<button type="reset" onclick="return submitbutton('cancelvenue')">
+  					<?php echo JText::_('CANCEL') ?>
+  				</button>
 				</div>
 
-				<div style="float: right;">
-				<button type="button validate" onclick="return submitbutton('savevenue')">
-					<?php echo JText::_('SAVE') ?>
-				</button>
-				<button type="reset" onclick="return submitbutton('cancelvenue')" />
-					<?php echo JText::_('CANCEL') ?>
-				</button>
+				<br class="clear" />
+
+      	<fieldset class="el_fldst_address">
+
+            <legend><?php echo JText::_('ADDRESS'); ?></legend>
+
+            <div class="el_venue floattext">
+                <label for="venue"><?php echo JText::_( 'VENUE' ).':'; ?></label>
+                <input class="inputbox required" type="text" name="venue" id="venue" value="<?php echo $this->escape($this->row->venue); ?>" size="55" maxlength="50" />
+            </div>
+
+            <div class="el_street floattext">
+                <label for="street"><?php echo JText::_( 'STREET' ).':'; ?></label>
+                <input class="inputbox validate-map" type="text" name="street" id="street" value="<?php echo $this->escape($this->row->street); ?>" size="55" maxlength="50" />
+            </div>
+
+            <div class="el_plz floattext">
+                <label for="plz"><?php echo JText::_( 'ZIP' ).':'; ?></label>
+                <input class="inputbox validate-map" type="text" name="plz" id="plz" value="<?php echo $this->escape($this->row->plz); ?>" size="15" maxlength="10" />
+            </div>
+
+            <div class="el_city floattext">
+                <label for="city"><?php echo JText::_( 'CITY' ).':'; ?></label>
+                <input class="inputbox validate-map" type="text" name="city" id="city" value="<?php echo $this->escape($this->row->city); ?>" size="55" maxlength="50" />
+            </div>
+
+            <div class="el_state floattext">
+                <label for="state"><?php echo JText::_( 'STATE' ).':'; ?></label>
+                <input class="inputbox" type="text" name="state" id="state" value="<?php echo $this->escape($this->row->state); ?>" size="55" maxlength="50" />
+            </div>
+
+            <div class="el_country floattext">
+                <label for="country"><?php echo JText::_( 'COUNTRY' ).':'; ?></label>
+                <input class="inputbox validate-map" type="text" name="country" id="country" value="<?php echo $this->row->country; ?>" size="4" maxlength="3" />
+                <span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('COUNTRY HINT'); ?>">
+                		<?php echo $this->infoimage; ?>
+                </span>
+            </div>
+
+            <div class="el_url floattext">
+                <label for="url"><?php echo JText::_( 'WEBSITE' ).':'; ?></label>
+                <input class="inputbox" name="url" id="url" type="text" value="<?php echo $this->escape($this->row->url); ?>" size="55" maxlength="150" />&nbsp;
+                <span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('WEBSITE HINT'); ?>">
+                		<?php echo $this->infoimage; ?>
+                </span>
+            </div>
+
+            <?php if ( $this->elsettings->showmapserv != 0 ) : ?>
+            <div class="el_map floattext">
+                <p>
+                    <br /><strong><?php echo JText::_( 'ENABLE MAP' ).':'; ?></strong>
+                    <span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('ADDRESS NOTICE'); ?>">
+                        <?php echo $this->infoimage; ?>
+                    </span>
+                </p>
+
+                <label for="map0">No</label>
+                <input type="radio" name="map" id="map0" value="0" class="inputbox" />
+                <br class="clear" />
+              	<label for="map1">Yes</label>
+              	<input type="radio" name="map" id="map1" value="1" checked="checked" class="inputbox" />
+            </div>
+            <?php endif; ?>
+
+        </fieldset>
+
+      	<?php	if (( $this->elsettings->imageenabled == 2 ) || ($this->elsettings->imageenabled == 1)) :	?>
+      	<fieldset class="el_fldst_image">
+
+            <legend><?php echo JText::_('IMAGE'); ?></legend>
+
+    				<?php
+            if ($this->row->locimage) :
+    					echo ELOutput::flyer( $this->row, $this->elsettings, $this->limage );
+    				else :
+      		    echo JHTML::_('image', 'components/com_eventlist/assets/images/noimage.png', JText::_('NO IMAGE'), array('class' => 'modal'));
+    				endif;
+      			?>
+
+            <label for="userfile"><?php echo JText::_('IMAGE'); ?></label>
+      			<input class="inputbox <?php echo $this->elsettings->imageenabled == 2 ? 'required' : ''; ?>" name="userfile" id="userfile" type="file" />
+      			<span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('MAX IMAGE FILE SIZE').' '.$this->elsettings->sizelimit.' kb'; ?>">
+      					<?php echo $this->infoimage; ?>
+      			</span>
+
+      			<!--<?php echo JText::_( 'CURRENT IMAGE' );	?>
+      			<?php echo JText::_( 'SELECTED IMAGE' ); ?>-->
+
+      	</fieldset>
+      	<?php endif; ?>
+
+      	<fieldset class="el_fldst_description">
+
+          	<legend><?php echo JText::_('DESCRIPTION'); ?></legend>
+
+        		<?php
+        		//wenn usertyp min editor wird editor ausgegeben ansonsten textfeld
+        		if ( $this->editoruser ) :
+        			echo $this->editor->display('locdescription', $this->row->locdescription, '655', '400', '70', '15', array('pagebreak', 'readmore') );
+        		else :
+        		?>
+      			<textarea style="width:100%;" rows="10" name="locdescription" class="inputbox" wrap="virtual" onkeyup="berechne(this.form)"></textarea><br />
+      			<?php echo JText::_('NO HTML'); ?><br />
+      			<input disabled value="<?php echo $this->elsettings->datdesclimit; ?>" size="4" name="zeige" /><?php echo JText::_('AVAILABLE')." "; ?><br />
+      			<a href="javascript:rechne(document.adminForm);"><?php echo JText::_('REFRESH'); ?></a>
+
+        		<?php	endif; ?>
+
+      	</fieldset>
+
+      	<fieldset class="el_fldst_meta">
+
+          	<legend><?php echo JText::_('METADATA INFORMATION'); ?></legend>
+
+            <div class="el_box_left">
+              	<label for="metadesc"><?php echo JText::_( 'META DESCRIPTION' ); ?></label>
+          			<textarea class="inputbox" cols="40" rows="5" name="meta_description" id="metadesc" style="width:300px;"></textarea>
+            </div>
+
+            <div class="el_box_right">
+        				<label for="metakey"><?php echo JText::_( 'META KEYWORDS' ); ?></label>
+        				<textarea class="inputbox" cols="40" rows="5" name="meta_keywords" id="metakey" style="width:300px;"></textarea>
+            </div>
+
+            <br class="clear" />
+    				<input type="button" class="button el_fright" value="<?php echo JText::_( 'ADD VENUE CITY' ); ?>" onclick="f=document.adminForm;f.metakey.value=f.venue.value+', '+f.city.value+f.metakey.value;" />
+
+      	</fieldset>
+
+      	<div class="el_save_buttons floattext">
+    				<button type="button" onclick="return submitbutton('savevenue')">
+    					<?php echo JText::_('SAVE') ?>
+    				</button>
+    				<button type="reset" onclick="return submitbutton('cancelvenue')">
+    					<?php echo JText::_('CANCEL') ?>
+    				</button>
 				</div>
-			</td>
-		</tr>
-	</table>
+				<br class="clear" />
 
-	<br />
+      	<input type="hidden" name="option" value="com_eventlist" />
+      	<input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
+      	<input type="hidden" name="returnview" value="<?php echo $this->returnview; ?>" />
+      	<input type="hidden" name="created" value="<?php echo $this->row->created; ?>" />
+      	<input type="hidden" name="curimage" value="<?php echo $this->row->locimage; ?>" />
+      	<?php echo JHTML::_( 'form.token' ); ?>
+      	<input type="hidden" name="task" value="" />
 
-	<fieldset>
-	<legend><?php echo JText::_('ADDRESS'); ?></legend>
+    </form>
 
-		<table class="adminform" width="100%">
-		  	<tr>
-  				<td><label for="street"><?php echo JText::_( 'STREET' ).':'; ?></label></td>
-				<td><input class="inputbox validate-map" type="text" name="street" id="street" value="<?php echo $this->escape($this->row->street); ?>" size="55" maxlength="50" /></td>
-		  	</tr>
-  			<tr>
-  			  	<td><label for="plz"><?php echo JText::_( 'ZIP' ).':'; ?></label></td>
-  			  	<td><input class="inputbox validate-map" type="text" name="plz" id="plz" value="<?php echo $this->escape($this->row->plz); ?>" size="15" maxlength="10" /></td>
-		  	</tr>
-  			<tr>
-  				<td><label for="city"><?php echo JText::_( 'CITY' ).':'; ?></label></td>
-  				<td><input class="inputbox validate-map" type="text" name="city" id="city" value="<?php echo $this->escape($this->row->city); ?>" size="55" maxlength="50" />
-				</td>
-  			</tr>
-  			<tr>
-  				<td><?php echo JText::_( 'STATE' ).':'; ?></td>
-  				<td><input class="inputbox" type="text" name="state" id="state" value="<?php echo $this->escape($this->row->state); ?>" size="55" maxlength="50" />
-				</td>
-  			</tr>
-  			<tr>
-  			  	<td><label for="country"><?php echo JText::_( 'COUNTRY' ).':'; ?></label></td>
-  			  	<td>
-					<input class="inputbox validate-map" type="text" name="country" id="country" value="<?php echo $this->row->country; ?>" size="4" maxlength="3" />&nbsp;
-					<span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('COUNTRY HINT'); ?>">
-						<?php echo $this->infoimage; ?>
-					</span>
-				</td>
-		  	</tr>
-  			<tr>
-    			<td><?php echo JText::_( 'WEBSITE' ).':'; ?></td>
-    			<td>
-    				<input class="inputbox" name="url" id="url" type="text" value="<?php echo $this->escape($this->row->url); ?>" size="55" maxlength="150" />&nbsp;
-    				<span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('WEBSITE HINT'); ?>">
-						<?php echo $this->infoimage; ?>
-					</span>
-    			</td>
-  			</tr>
-  			<?php if ( $this->elsettings->showmapserv != 0 ) : ?>
-			<tr>
-				<td>
-					<label for="map">
-						<?php echo JText::_( 'ENABLE MAP' ).':'; ?>
-					</label>
-				</td>
-				<td>
-					<?php echo JHTML::_('select.booleanlist', 'map', 'class="inputbox"', $this->row->map ); ?>
-          			&nbsp;
-          			<span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('ADDRESS NOTICE'); ?>">
-						<?php echo $this->infoimage; ?>
-					</span>
-				</td>
-			</tr>
-			<?php endif; ?>
-		</table>
+    <p class="copyright">
+        <?php echo ELOutput::footer( ); ?>
+    </p>
 
-	</fieldset>
-
-	<?php
-		if (( $this->elsettings->imageenabled == 2 ) || ($this->elsettings->imageenabled == 1)) :
-	?>
-
-	<fieldset>
-	<legend><?php echo JText::_('IMAGE'); ?></legend>
-
-	<table class="adminform" width="100%">
-		<tr>
-			<td>
-				<?php
-				if ($this->row->locimage) :
-					echo ELOutput::flyer( $this->row, $this->elsettings, $this->limage );
-				 else :
-				 	echo JHTML::_('image', 'images/cancel.png', 'no image');
-				endif;
-  			  	?>
-			</td>
-			<td>
-				<input class="inputbox <?php echo $this->elsettings->imageenabled == 2 ? 'required' : ''; ?>" name="userfile" id="userfile" type="file" />&nbsp;
-				<span class="editlinktip hasTip" title="<?php echo JText::_( 'NOTES' ); ?>::<?php echo JText::_('MAX IMAGE FILE SIZE').' '.$this->elsettings->sizelimit.' kb'; ?>">
-						<?php echo $this->infoimage; ?>
-				</span>
-			</td>
-		</tr>
-		<tr>
-			<td><?php echo JText::_( 'CURRENT IMAGE' );	?></td>
-			<td><?php echo JText::_( 'SELECTED IMAGE' ); ?></td>
-		</tr>
-	</table>
-	</fieldset>
-	<?php endif; ?>
-
-	<fieldset>
-	<legend><?php echo JText::_('DESCRIPTION'); ?></legend>
-		<?php
-		//wenn usertyp min editor wird editor ausgegeben ansonsten textfeld
-		if ( $this->editoruser ) :
-			echo $this->editor->display('locdescription', $this->row->locdescription, '655', '400', '70', '15', array('pagebreak', 'readmore') );
-		else :
-		?>
-			<textarea style="width:100%;" rows="10" name="locdescription" class="inputbox" wrap="virtual" onkeyup="berechne(this.form)"></textarea><br />
-			<?php echo JText::_('NO HTML'); ?><br />
-			<input disabled value="<?php echo $this->elsettings->datdesclimit; ?>" size="4" name="zeige" /><?php echo JText::_('AVAILABLE')." "; ?><br />
-			<a href="javascript:rechne(document.adminForm);"><?php echo JText::_('REFRESH'); ?></a>
-
-		<?php
-		endif;
-		?>
-
-	</fieldset>
-
-	<fieldset>
-	<legend><?php echo JText::_('METADATA INFORMATION'); ?></legend>
-
-	<table>
-		<tr>
-			<td valign="top">
-				<label for="metadesc">
-					<?php echo JText::_( 'META DESCRIPTION' ); ?>:
-				</label>
-				<br />
-				<textarea class="inputbox" cols="40" rows="5" name="meta_description" id="metadesc" style="width:300px;"></textarea>
-			</td>
-			<td valign="top">
-				<label for="metakey">
-					<?php echo JText::_( 'META KEYWORDS' ); ?>:
-				</label>
-				<br />
-				<textarea class="inputbox" cols="40" rows="5" name="meta_keywords" id="metakey" style="width:300px;"></textarea>
-				<br />
-				<input type="button" class="button" value="<?php echo JText::_( 'ADD VENUE CITY' ); ?>" onclick="f=document.adminForm;f.metakey.value=f.venue.value+', '+f.city.value+f.metakey.value;" />
-			</td>
-		</tr>
-	</table>
-	</fieldset>
-
-
-	<input type="hidden" name="option" value="com_eventlist" />
-	<input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
-	<input type="hidden" name="returnview" value="<?php echo $this->returnview; ?>" />
-	<input type="hidden" name="created" value="<?php echo $this->row->created; ?>" />
-	<input type="hidden" name="curimage" value="<?php echo $this->row->locimage; ?>" />
-	<?php echo JHTML::_( 'form.token' ); ?>
-	<input type="hidden" name="task" value="" />
-</form>
-
-<p class="copyright">
-	<?php echo ELOutput::footer( ); ?>
-</p>
+</div>
 
 <?php
 //keep session alive while editing
