8028c5a1f516c41e001c5f771d93d3c28cab4830
julienV <julien.vonthron@gmail.com>
2008-12-08 22:12:56 +0000

    added parameter to hide or show empty subcats in categoryevents.

--- views/categoryevents/tmpl/default_subcategories.php
+++ views/categoryevents/tmpl/default_subcategories.php
@@ -31,11 +31,13 @@ $i = 0;
 ?>
 <div class="subcategorieslist">
 	<?php foreach ($this->categories as $sub) : ?>
-		<strong><a href="<?php echo JRoute::_( 'index.php?view=categoryevents&id='. $sub->slug ); ?>"><?php echo $this->escape($sub->catname); ?></a></strong> (<?php echo $sub->assignedevents != null ? $sub->assignedevents : 0; ?>)
-		<?php 
-		$i++;
-		if ($i != $n) :
-			echo ',';
+    <?php if ($this->params->get('show_empty', 1) || $sub->assignedevents != null): ?>
+			<strong><a href="<?php echo JRoute::_( 'index.php?view=categoryevents&id='. $sub->slug ); ?>"><?php echo $this->escape($sub->catname); ?></a></strong> (<?php echo $sub->assignedevents != null ? $sub->assignedevents : 0; ?>)
+			<?php 
+			$i++;
+			if ($i != $n) :
+				echo ',';
+			endif;
 		endif;
 	endforeach; ?>
 </div>
