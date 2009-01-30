ef960df35db0a8d3e07e23a9a1e6f06bfb63c1c6
schlu <schlu@users.sourceforge.net>
2009-01-30 06:03:57 +0000

    year bump

--- admin/views/import/tmpl/default.php
+++ admin/views/import/tmpl/default.php
@@ -3,7 +3,7 @@
  * @version 1.0 $Id: default.php 662 2008-05-09 22:28:53Z schlu $
  * @package Joomla
  * @subpackage EventList
- * @copyright (C) 2005 - 2008 Christoph Lukes
+ * @copyright (C) 2008 - 2009 Christoph Lukes
  * @license GNU/GPL, see LICENSE.php
  * EventList is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 2
