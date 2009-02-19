78103ed0b2244cbe710b0808c0250f6c69d4624f
schlu <schlu@users.sourceforge.net>
2009-02-19 21:54:35 +0000

    prevent loading outside of the framework

--- classes/calendar.class.php
+++ classes/calendar.class.php
@@ -50,6 +50,8 @@
  * The layout of can be configured using css, as the class generates various html classes
  * Please read the readme.html first and check the examples included in this package
  */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
 class ELCalendar{
 /*
 ----------------------
